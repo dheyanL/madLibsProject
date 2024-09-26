d890b9849d7d2fc1c16ed8b695c816b15b757abd
dlim <dheyan.lim@student.tamdistrict.org>
2024-09-26 09:11:33 -0700

    added madLibs.java and main

---
+++ madLibs.java
@@ -0,0 +1,9 @@
+public class madLibs {
+    public static void main(String[] args) {
+        
+    }
+    public String construct(String a, String b){
+        String constructed= a+ " "+b;
+        return constructed;
+    }
+}
